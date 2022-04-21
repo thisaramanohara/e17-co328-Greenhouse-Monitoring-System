171f6bdc8a3306df4c8f98b2fc62d53c2f2acfae
thisaramanohara <thisaramanohara@gmail.com>
2022-04-21 12:27:14 +0530

    Session removed

--- backend/app.js
+++ backend/app.js
@@ -40,13 +40,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
